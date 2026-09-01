76d7bb1164170028bb8679a5f632b78d8a6d559b
Lauro Golemba Junior <golembalauro@gmail.com>
2026-09-01 19:50:25 -0300

    Fim do teste

--- Apresentacao.java
+++ Apresentacao.java
@@ -1,5 +1,5 @@
 public class Apresentacao {
     public static void main(String[] args) {
-        System.out.println("Teste - Hackeado por Lauro!");
+        System.out.println("Olá, meu nome é XXXX! Missão 00 concluída com sucesso.");
     }
 }
